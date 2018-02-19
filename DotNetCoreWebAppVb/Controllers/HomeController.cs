035409dd27112e8c47ab9a3ed2376c374a0859c9
ernestoCasteur <ernesto.casteur@student.howest.be>
2018-02-19 11:56:43 +0100

    Groenten

--- DotNetCoreWebAppVb/Controllers/HomeController.cs
+++ DotNetCoreWebAppVb/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-﻿    using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,9 +8,23 @@ namespace DotNetCoreWebAppVb.Controllers
 {
     public class HomeController : Controller
     {
-        public String Index(string name)
+        private string[] groenten = { "Rode kool", "Spruitjes", "Wortel", "Spinazie" };
+
+        public ViewResult Index(string name)
         {
-            return $"Hallo {name}";
+            ViewBag.Groet = DateTime.Now.Hour < 12 ? "Goedemorgen" : "GoedeMiddag";
+            return View();
+        }
+
+        public ViewResult Groenten(string zoekGroente)
+        {
+            ViewBag.Groenten = groenten;
+            if (!string.IsNullOrEmpty(zoekGroente))
+            {
+                ViewBag.Zoekresultaat = $"de gezochte groente is {Array.IndexOf(groenten, zoekGroente) + 1}e uit de lijst ";
+
+            }
+            return View();
         }
     }
 }
